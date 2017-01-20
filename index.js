44947134edc7beaa9f69edd7658f4a4fd063a85d
Mac <Mac@Macs-MacBook-Air.local>
2017-01-19 19:36:35 -0500

    updated the server file to serve a static html file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var server  = express();
 
 var port = 8080;
 
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root:__dirname + '/public/html'});
+});
 
 server.listen(port, function(){
     console.log('Now listening on port', port);
